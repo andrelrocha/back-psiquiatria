673384e6ced298291d826af3a00ce641d4f9c8a0
andrelrocha <andrerocha0911@gmail.com>
2023-10-11 22:14:03 -0300

    feat: get diretrizes by doenca

---
+++ src/main/java/jr/acens/api/domain/diretriz/useCase/GetDiretrizesByDoencaUseCase.java
@@ -0,0 +1,24 @@
+package jr.acens.api.domain.diretriz.useCase;
+
+import jr.acens.api.domain.DoencasConverter;
+import jr.acens.api.domain.diretriz.DTO.DiretrizReturnDTO;
+import jr.acens.api.domain.diretriz.DiretrizRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class GetDiretrizesByDoencaUseCase {
+    @Autowired
+    private DiretrizRepository repository;
+    @Autowired
+    private DoencasConverter converter;
+
+    public Page<DiretrizReturnDTO> getDiretrizesByDoenca(Pageable pageable, String doenca) {
+        var doencaEnum = converter.convert(doenca);
+
+        return repository.findAllDiretrizesByDoenca(pageable, doencaEnum).map(DiretrizReturnDTO::new);
+    }
+}
